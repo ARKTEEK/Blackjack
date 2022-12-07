e8fb6d85c4df6ffdecaec507c3d1ad307ff9ebcf
ZenDev <minde398@gmail.com>
2022-12-07 15:18:49 +0200

    Added main files.

---
+++ script.js
@@ -0,0 +1,133 @@
+var dealerCardsSum = 0;
+var playerCardsSum = 0;
+
+var hiddenCard;
+var deck;
+
+var stayed = false;
+
+window.onload = function() {
+    createDeck();
+    shuffleDeck();
+    drawCards();
+}
+
+function createDeck() {
+    let cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let cardTypes = ["C", "D", "H", "S"];
+    deck = [];
+
+    for (let i = 0; i < cardTypes.length; i++) {
+        for (let j = 0; j < cardValues.length; j++) {
+            deck.push(cardValues[j] + "-" + cardTypes[i]);
+        }
+    }
+}
+
+function shuffleDeck() {
+
+    let currentIndex = deck.length, randomIndex;
+
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [deck[currentIndex], deck[randomIndex]] = [deck[randomIndex], deck[currentIndex]];
+
+    }
+    
+}
+
+function drawCards() {
+    hiddenCard = deck.pop();
+    dealerCardsSum += getCardValue(hiddenCard);
+    
+    while (dealerCardsSum < 17) {
+        let cardImage = document.createElement("img");
+        let card = deck.pop();
+        cardImage.src = "./cards/" + card + ".png";
+        dealerCardsSum += getCardValue(card);
+        document.getElementById("dealer-cards").append(cardImage);
+    }
+
+    for (let i = 0; i < 2; i++) {
+        let cardImage = document.createElement("img");
+        let card = deck.pop();
+        cardImage.src = "./cards/" + card + ".png";
+        playerCardsSum += getCardValue(card);
+        document.getElementById("player-cards").append(cardImage);
+    }
+
+    document.getElementById("hit").addEventListener("click", hit);
+    document.getElementById("stay").addEventListener("click", stay);
+    document.getElementById("restart").addEventListener("click", restart);
+}
+
+function stay() {
+    document.getElementById("hidden_card").src = "./cards/" + hiddenCard + ".png";
+    stayed = true;
+    checkGame();
+    document.getElementById("dealer-cards-sum").innerText = dealerCardsSum;
+    document.getElementById("player-cards-sum").innerText = playerCardsSum;
+    document.getElementById("restart").classList.remove("hidden");
+    
+}
+
+function restart() {
+    window.location.reload();
+}
+
+function checkGame() {
+
+    let gameResult = "";
+    
+    if (playerCardsSum > 21) {
+        gameResult = "LOSE!";
+    } else if (dealerCardsSum > 21) {
+        gameResult = "WIN!";
+    } else if (playerCardsSum > dealerCardsSum) {
+        gameResult = "WIN!";
+    } else if (playerCardsSum < dealerCardsSum) {
+        gameResult = "LOSE!";
+    } else if (playerCardsSum == dealerCardsSum) {
+        gameResult = "TIE!";
+    }
+
+    document.getElementById("result").innerText = gameResult;
+
+}
+
+function hit() {
+
+    if (stayed) return;
+
+    if (canHit()) {
+        stay();
+        return;
+    }
+
+    let cardImage = document.createElement("img");
+    let card = deck.pop();
+    
+    cardImage.src = "./cards/" + card + ".png";
+    playerCardsSum += getCardValue(card);
+    document.getElementById("player-cards").append(cardImage);
+}
+
+function getCardValue(card) {
+    let cardValue = card.split("-");
+    let value = cardValue[0];
+
+    if (isNaN(value)) {
+        if (value == "A") {
+            return 11;
+        }
+        return 10;
+    }
+
+    return parseInt(cardValue);
+}
+
+function canHit() {
+    return playerCardsSum > 21;
+}
